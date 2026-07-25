5d67c73efd3994fd536b904c5e35a8b575040222
ES <2662442385@qq.com>
2026-07-25 15:34:50 +0800

    chore: desc to en

--- .config/nvim/lua/plugins/manager/commands.lua
+++ .config/nvim/lua/plugins/manager/commands.lua
@@ -3,4 +3,4 @@ vim.api.nvim_create_user_command('PluginManager', function()
   require('plugins.manager').open()
 end, { desc = '打开可选插件管理（勾选/取消后保存）' })
 
-vim.keymap.set('n', '<leader>fp', '<cmd>PluginManager<cr>', { desc = '可选插件管理' })
+vim.keymap.set('n', '<leader>fp', '<cmd>PluginManager<cr>', { desc = 'Plugin Manager' })
